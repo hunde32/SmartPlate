7506fe3912816046985c689c6f2c84aa708c28ce
hunde32 <hundeman3223@gmail.com>
2025-08-07 21:20:55 +0300

    AI intgration started

--- src/pages/Aiplanner.jsx
+++ src/pages/Aiplanner.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
 const Aiassistant = () => {
+  const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+  const run = async (prompt) =>{
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+      try{
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text)
+      return text;
+      }catch{
+        console.error("API call failed:", error);
+        return "Error: Could not get a response from the Gemini API.";
+      }
+  }
+  run("does this gemini api remember old texts")
   return (
-    <div>Aiassistant</div>
+    <div>
+
+    </div>
   )
 }
 
